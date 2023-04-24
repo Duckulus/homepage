3fd2e8ededce71bfc70ae9968cb6c0c16be2cee6
Amin <amin.haddou@frg.wwschool.de>
2023-04-24 12:23:46 +0200

    reformat code

--- src/main/kotlin/us/duckul/homepage/plugins/Routing.kt
+++ src/main/kotlin/us/duckul/homepage/plugins/Routing.kt
@@ -2,7 +2,6 @@ package us.duckul.homepage.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
 
