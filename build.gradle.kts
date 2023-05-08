3bb44f9d9bf3d9259c1a0cfd10f318e9a7822af2
Amin <amin.haddou@frg.wwschool.de>
2023-05-08 11:52:40 +0200

    add 404 page

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,11 @@ dependencies {
     implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-host-common-jvm:2.3.0")
+    implementation("io.ktor:ktor-server-status-pages-jvm:2.3.0")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/us/duckul/homepage/plugins/Routing.kt
+++ src/main/kotlin/us/duckul/homepage/plugins/Routing.kt
@@ -1,16 +1,24 @@
 package us.duckul.homepage.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.routing.*
 import java.io.File
 
 fun Application.configureRouting() {
+    routing {
+        staticFiles("/", File("./static"))
+    }
     install(IgnoreTrailingSlash) {
         routing {
             staticFiles("/", File("./static")) {
                 extensions("html", "htm")
             }
         }
+    }
+    install(StatusPages) {
+        statusFile(HttpStatusCode.NotFound, filePattern = "error#.html")
     }
 }
