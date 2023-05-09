eaf0fc77ed0d4ccffb7699f8a4aadc03b2ab7194
Amin <amin.haddou@frg.wwschool.de>
2023-05-09 15:05:20 +0200

    add misc routes

--- src/main/kotlin/us/duckul/homepage/plugins/Routing.kt
+++ src/main/kotlin/us/duckul/homepage/plugins/Routing.kt
@@ -3,13 +3,25 @@ package us.duckul.homepage.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
 
 fun Application.configureRouting() {
     routing {
         staticFiles("/", File("./static"))
+        staticFiles("/files", File("./files"))
+        get("/hello") {
+            call.respondText("Hello, world!")
+        }
+        get("/ip") {
+            call.respondText(call.request.origin.remoteHost)
+        }
+        get("/time") {
+            call.respondText(System.currentTimeMillis().toString())
+        }
     }
     install(IgnoreTrailingSlash) {
         routing {
