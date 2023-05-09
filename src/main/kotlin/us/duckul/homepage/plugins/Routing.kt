85f6795998a1263f956898e704f16c2e1e97d8dd
Amin <amin.haddou@frg.wwschool.de>
2023-05-09 21:53:22 +0200

    fix ip when behind cloudflare

--- src/main/kotlin/us/duckul/homepage/plugins/Routing.kt
+++ src/main/kotlin/us/duckul/homepage/plugins/Routing.kt
@@ -3,7 +3,6 @@ package us.duckul.homepage.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
-import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -17,7 +16,7 @@ fun Application.configureRouting() {
             call.respondText("Hello, world!")
         }
         get("/ip") {
-            call.respondText(call.request.origin.remoteAddress)
+            call.respondText(call.request.headers["cf-connecting-ip"] ?: call.request.local.remoteAddress)
         }
         get("/time") {
             call.respondText(System.currentTimeMillis().toString())
